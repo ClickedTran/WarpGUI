eccd0de0c8d49e606f26c9c727d7fa5eeba38756
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-04-26 23:49:30 +0700

    Update EventListener.php

--- src/ClickedTran/WarpGUI/EventListener.php
+++ src/ClickedTran/WarpGUI/EventListener.php
@@ -16,7 +16,7 @@ class EventListener implements Listener {
          $this->plugin = $plugin;
     }
   
-    public function onChat(PlayerChatEvent $event): void{
+    public function onChat(PlayerChatEvent $event){
 		$player = $event->getPlayer();
 		if(isset($this->plugin->editwarp[$player->getName()])){
 			$event->cancel();
