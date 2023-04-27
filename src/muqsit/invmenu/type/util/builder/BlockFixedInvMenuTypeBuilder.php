32625a067844edf48ab4ebcb56993f58b3ea7d04
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-04-27 21:20:21 +0700

    Add files via upload

--- src/muqsit/invmenu/type/util/builder/BlockFixedInvMenuTypeBuilder.php
+++ src/muqsit/invmenu/type/util/builder/BlockFixedInvMenuTypeBuilder.php
@@ -5,25 +5,18 @@ declare(strict_types=1);
 namespace muqsit\invmenu\type\util\builder;
 
 use muqsit\invmenu\type\BlockFixedInvMenuType;
-
 use muqsit\invmenu\type\graphic\network\BlockInvMenuGraphicNetworkTranslator;
 
 final class BlockFixedInvMenuTypeBuilder implements InvMenuTypeBuilder{
-
-	use BlockInvMenuTypeBuilderTrait;	use FixedInvMenuTypeBuilderTrait;
-
+	use BlockInvMenuTypeBuilderTrait;
+	use FixedInvMenuTypeBuilderTrait;
 	use GraphicNetworkTranslatableInvMenuTypeBuilderTrait;
 
 	public function __construct(){
-
 		$this->addGraphicNetworkTranslator(BlockInvMenuGraphicNetworkTranslator::instance());
-
 	}
 
 	public function build() : BlockFixedInvMenuType{
-
 		return new BlockFixedInvMenuType($this->getBlock(), $this->getSize(), $this->getGraphicNetworkTranslator());
-
 	}
-
 }
