8e1269c65c208b6de0d603b0568c9b6626c6474b
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-07-15 01:01:30 +0700

    Update EventListener.php

--- src/ClickedTran/WarpGUI/EventListener.php
+++ src/ClickedTran/WarpGUI/EventListener.php
@@ -18,10 +18,10 @@ class EventListener implements Listener {
   
     public function onChat(PlayerChatEvent $event){
 		$player = $event->getPlayer();
-		if(isset($this->plugin->editwarp[$player->getName()])){
+		if(isset(WarpGUI::getInstance()->editwarp[$player->getName()])){
 			$event->cancel();
 			$args = explode(" ", $event->getMessage());
-			$warp = $this->plugin->editwarp[$player->getName()];
+			$warp = WarpGUI::getInstance()->editwarp[$player->getName()];
 			switch($args[0]){
 				case "help":
 				case "?":
@@ -39,14 +39,14 @@ class EventListener implements Listener {
 				    $y = $player->getPosition()->getY();
 				    $z = $player->getPosition()->getZ();
 				    $world = $player->getPosition()->getWorld()->getDisplayName();
-				    if($this->plugin->getWarp()->exists($warp)){
-				        $this->plugin->getWarp()->set($warp, [
+				    if(WarpGUI::getInstance()->getWarp()->exists($warp)){
+				        WarpGUI::getInstance()->getWarp()->set($warp, [
 				          "position" => "$x $y $z",  
 				          "world" => "$world",
-				          "item" => $this->plugin->getWarp()->get($warp)["item"],        
-				          "slot" => $this->plugin->getWarp()->get($warp)["slot"]
+				          "item" => WarpGUI::getInstance()->getWarp()->get($warp)["item"],        
+				          "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
 		               ]);
-		               $this->plugin->getWarp()->save();
+		               WarpGUI::getInstance()->getWarp()->save();
 		               $player->sendMessage("§aSuccessfully updated position in X: $x Y: $y Z: $z World: $world");
 		            }else{
 		            	$player->sendMessage("§cThe warp you are editing no longer exists in the data");
@@ -57,22 +57,22 @@ class EventListener implements Listener {
 		            	$player->sendMessage("§cUsage:§7 name <name>");
 		            	return;
 		            }
-		            if(!$this->plugin->getWarp()->exists($args[1])){
-		                $ex = explode(" ", $this->plugin->getPositionWarps($warp));
+		            if(!WarpGUI::getInstance()->getWarp()->exists($args[1])){
+		                $ex = explode(" ", WarpGUI::getInstance()->getPositionWarps($warp));
 		                $x = (int)$ex[0];
 		                $y = (int)$ex[1];
 		                $z = (int)$ex[2];
-		                $world = $this->plugin->getWorldWarps($warp);
-				$this->plugin->getWarp()->set($args[1], [
-                                     "position" => $this->plugin->getWarp()->get($warp)["position"],
-                                     "world" => $this->plugin->getWarp()->get($warp)["world"],
-                                     "item" => $this->plugin->getWarp()->get($warp)["item"],
-                                     "slot" => $this->plugin->getWarp()->get($warp)["slot"]
+		                $world = WarpGUI::getInstance()->getWorldWarps($warp);
+				WarpGUI::getInstance()->getWarp()->set($args[1], [
+                                     "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
+                                     "world" => WarpGUI::getInstance()->getWarp()->get($warp)["world"],
+                                     "item" => WarpGUI::getInstance()->getWarp()->get($warp)["item"],
+                                     "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
 				]);
-				$this->plugin->getWarp()->save();
-				$this->plugin->removeWarp($warp);
-		                unset($this->plugin->editwarp[$player->getName()]);
-		                $this->plugin->editwarp[$player->getName()] = $args[1];
+				WarpGUI::getInstance()->getWarp()->save();
+				WarpGUI::getInstance()->removeWarp($warp);
+		                unset(WarpGUI::getInstance()->editwarp[$player->getName()]);
+		                WarpGUI::getInstance()->editwarp[$player->getName()] = $args[1];
 		                $player->sendMessage("§aSuccessfully rename warp to $args[1]");
 		            }else{
                                 $player->sendMessage("§cWarp name $args[1] is already exists");
@@ -82,13 +82,13 @@ class EventListener implements Listener {
                     if($player->getInventory()->getItemInHand()->getId() > 0){
                     	$id = $player->getInventory()->getItemInHand()->getId();
                     	$meta = $player->getInventory()->getItemInHand()->getMeta();
-                    	$this->plugin->getWarp()->set($warp, [
-                            "position" => $this->plugin->getWarp()->get($warp)["position"],
-                            "world" => $this->plugin->getWarp()->get($warp)["world"],
+                    	WarpGUI::getInstance()->getWarp()->set($warp, [
+                            "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
+                            "world" => WarpGUI::getInstance()->getWarp()->get($warp)["world"],
                             "item" => $id.":".$meta,
-                            "slot" => $this->plugin->getWarp()->get($warp)["slot"]
+                            "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
                     	]);
-                        $this->plugin->getWarp()->save();
+                        WarpGUI::getInstance()->getWarp()->save();
                     	$player->sendMessage("§aSuccessfully update item warp in gui");
                     }else{
                     	$player->sendMessage("§cPlease hold an item in your hand");
@@ -102,14 +102,14 @@ class EventListener implements Listener {
 		                }
 		                if($args[1] >= 0 || $args[1] <= 53){
 		                	$slot = (int)$args[1];
-		                    if(!$this->plugin->getWarp()->exists($args[1])){
-		                        $this->plugin->getWarp()->set($warp, [
-                                             "position" => $this->plugin->getWarp()->get($warp)["position"],
-                                             "world" => $this->plugin->getWarp()->get($warp)["world"],
-                                             "item" => $this->plugin->getWarp()->get($warp)["item"],
+		                    if(!WarpGUI::getInstance()->getWarp()->exists($args[1])){
+		                        WarpGUI::getInstance()->getWarp()->set($warp, [
+                                             "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
+                                             "world" => WarpGUI::getInstance()->getWarp()->get($warp)["world"],
+                                             "item" => WarpGUI::getInstance()->getWarp()->get($warp)["item"],
                                              "slot" => $slot
                             	        ]);
-					$this->plugin->getWarp()->save();
+					WarpGUI::getInstance()->getWarp()->save();
 		                        $player->sendMessage("§aSuccessfully update slot item in warp to $slot");
 		                    }else{
                                        $player->sendMessage("§cThe warp you are editing no longer exists in the data");
@@ -122,8 +122,8 @@ class EventListener implements Listener {
 		            }
                 break;
                 case "done":
-                    if(isset($this->plugin->editwarp[$player->getName()])){
-                    	unset($this->plugin->editwarp[$player->getName()]);
+                    if(isset(WarpGUI::getInstance()->editwarp[$player->getName()])){
+                    	unset(WarpGUI::getInstance()->editwarp[$player->getName()]);
                     	$player->sendMessage("§aYou have successfully left edit mode");
                     }
                 break;
