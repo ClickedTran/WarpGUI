8e640d5d95644256e0a8f63154f261dabfa92dd3
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-09-14 01:08:23 +0700

    Update WarpGUICommands.php

--- src/ClickedTran/WarpGUI/commands/WarpGUICommands.php
+++ src/ClickedTran/WarpGUI/commands/WarpGUICommands.php
@@ -5,129 +5,43 @@ declare(strict_types=1);
 namespace ClickedTran\WarpGUI\commands;
 
 use pocketmine\player\Player;
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginOwned;
+use pocketmine\Server;
+
+use CortexPE\Commando\BaseCommand;
+
 use ClickedTran\WarpGUI\WarpGUI;
 use ClickedTran\WarpGUI\gui\GUIManager;
+use ClickedTran\WarpGUI\commands\subcmds\HelpWarpCommand;
+use ClickedTran\WarpGUI\commands\subcmds\ListWarpCommand;
+use ClickedTran\WarpGUI\commands\subcmds\RemoveWarpCommand;
+use ClickedTran\WarpGUI\commands\subcmds\EditWarpCommand;
+use ClickedTran\WarpGUI\commands\subcmds\CreateWarpCommand;
 
-class WarpGUICommands extends Command implements PluginOwned {
+class WarpGUICommands extends BaseCommand {
 
     private WarpGUI $plugin;
     public $warp;
-
-    public function __construct(WarpGUI $plugin){
-    	$this->plugin = $plugin;
-    	parent::__construct("warpgui", "WarpGUI Commands", null, ["warp"]);
-    	$this->setPermission("warpgui.command");
+    
+    public function prepare(): void{
+      $this->setPermission("warpgui.command");
+      $this->registerSubcommand(new CreateWarpCommand("create", "§o§7Create New Warp Command"));
+      $this->registerSubcommand(new HelpWarpCommand("help", "§o§7Help WarpGUI Command"));
+      $this->registerSubcommand(new EditWarpCommand("edit", "§o§7Edit Warp Command"));
+      $this->registerSubcommand(new RemoveWarpCommand("remove", "§o§7Remoge Warp Command"));
+      $this->registerSubcommand(new ListWarpCommand("list", "§o§7List Warp Command"));
     }
-
-    public function execute(CommandSender $sender, string $label, array $args){
-    	if(!$sender instanceof Player){
-    		$sender->sendMessage("§cPlease use command in-game");
-    		return true;
-    	}
-    	if(!isset($args[0])){
-    	  $gui = new GUIManager();
-        $gui->menuWarpGUI($sender);
-    	}else{
-    		switch($args[0]){
-    			case "help":
-    			case "?":
-    			    if($sender->hasPermission("warpgui.command.help")){
-                        $sender->sendMessage("§6> All WarpGUI Commands <");
-                        $sender->sendMessage("§b/warpgui help §7- Display all command WarpGUI");
-                        $sender->sendMessage("§b/warpgui create §7- Add warp to data");
-                        $sender->sendMessage("§b/warpgui remove §7- Remove warp in data");
-                        $sender->sendMessage("§b/warpgui edit §7- Edit warp in data");
-    			    }else{
-                        $sender->sendMessage("§cYou no permission to use this command!");
-                    }
-    			break;
-    			case "create":
-                        case "add":
-                        case "new":
-			case "createwarp":
-                        case "addwarp":
-                        case "newwarp":
-    			    if($sender->hasPermission("warpgui.command.create")){
-                        if(!isset($args[1])){
-                        	$sender->sendMessage("§cUsage:§7 /warpgui setwarp <warp name>");
-                        	return true;
-                        }
-                        if($this->getOwningPlugin()->warp->exists($args[1])){
-                            $sender->sendMessage("§cWarp name $args[1] is already exists !");
-                            return true;
-                        }
-                        $this->getOwningPlugin()->addWarp($args[1]);
-                        $sender->sendMessage("§aSuccessfully created warp with name $args[1]");
-    			    }else{
-                        $sender->sendMessage("§cYou no permission to use this command!");
-                    }
-    			break;
-			case "remove":
-			case "delete":
-    			case "deletewarp":
-                        case "removewarp":
-                        case "delwarp":
-                        case "rmwarp":
-                        case "delete":
-                        case "del":
-                        case "remove":
-                        case "rm":
-    			    if($sender->hasPermission("warpgui.command.remove")){
-                        if(!isset($args[1])){
-                            $sender->sendMessage("§cUsage:§7 /warpgui setwarp <warp name>");
-                            return true;
-                        }
-                        if(!$this->getOwningPlugin()->warp->exists($args[1])){
-                            $sender->sendMessage("§cWarp name $args[1] not found !");
-                            return true;
-                        }
-                        $this->getOwningPlugin()->removeWarp($args[1]);
-                        $sender->sendMessage("§aSuccessfully removed warp with name $args[1]");
-    			    }else{
-                        $sender->sendMessage("§cYou no permission to use this command!");
-                    }
-    			break;
-    			case "edit":
-                case "setup":
-                case "editwarp":
-    			    if($sender->hasPermission("warpgui.command.edit")){
-                        if(!isset($args[1])){
-                            $sender->sendMessage("§cUsage:§7 /warpgui editwarp <warp name>");
-                            return true;
-                        }
-                        if(!$this->getOwningPlugin()->getWarp()->exists($args[1])){
-                            $sender->sendMessage("§cWarp name $args[1] not found !");
-                            return true;
-                        }
-                        $this->getOwningPlugin()->editwarp[$sender->getName()] = $args[1];
-                        $sender->sendMessage("§6You are in edit mode, usage:");
-                        $sender->sendMessage("§l§7help§r§7 - Display available commands");
-                        $sender->sendMessage("§l§7done§r§7 - save and leave edit mode");
-    			    }else{
-                        $sender->sendMessage("§cYou no permission to use this command!");
-                    }
-    			break;
-                case "warp":
-                    $gui = new GUIManager();
-                    $gui->menuWarpGUI($sender);
-                break;
-                default:
-                    if($sender->hasPermission("warpgui.command.help")){
-                        $sender->sendMessage("§cUsage:§7 /warpgui help");
-                        $sender->sendMessage("§cIf you want to open the warp menu, use the command:§7 /warpgui warp");
-                    }else{
-                        $gui = new GUIManager();
-                        $gui->menuWarpGUI($sender);
-                    }
-                break;
-    		}
-    	}
-    }
-
-    public function getOwningPlugin() : WarpGUI {
-        return $this->plugin;
+    
+    public function onRun(CommandSender $sender, String $aliasUsed, Array $args): void{
+      if(!$sender instanceof Player){
+        $sender->sendMessage("§cPlease use in-game!");
+        return;
+      }
+      if(count($args) === 0){
+        if(!isset($args[0])){
+            $gui = new GUIManager();
+            $gui->menuWarpGUI($sender);
+        }
+      }
     }
 }
