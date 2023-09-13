d51e48fd2396c03ffa98f81ee6a4d4d8a87afc11
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-09-14 01:04:01 +0700

    Fixed an issue where items were not added to slot 0

--- src/ClickedTran/WarpGUI/EventListener.php
+++ src/ClickedTran/WarpGUI/EventListener.php
@@ -51,9 +51,9 @@ class EventListener implements Listener {
                                              "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
 		                        ]);
 		                WarpGUI::getInstance()->getWarp()->save();
-		                $player->sendMessage("§aSuccessfully updated position in X: $x Y: $y Z: $z World: $world");
+		                $player->sendMessage("§9[ §aSuccessfully §9]§r§e You updated position in X: $x Y: $y Z: $z World: $world");
 		            }else{
-		            	$player->sendMessage("§cThe warp you are editing no longer exists in the data");
+		            	$player->sendMessage("§9[ §4ERROR §9]§r§c The warp you are editing no longer exists in the data");
 		            }
 		        break;
 		        case "name":
@@ -67,24 +67,24 @@ class EventListener implements Listener {
 		                $y = (int)$ex[1];
 		                $z = (int)$ex[2];
 		                $world = WarpGUI::getInstance()->getWorldWarps($warp);
-				WarpGUI::getInstance()->getWarp()->set($args[1], [
+			    	WarpGUI::getInstance()->getWarp()->set($args[1], [
                                      "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
                                      "world" => WarpGUI::getInstance()->getWarp()->get($warp)["world"],
                                      "item" => WarpGUI::getInstance()->getWarp()->get($warp)["item"],
                                      "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
-				]);
-				WarpGUI::getInstance()->getWarp()->save();
-				WarpGUI::getInstance()->removeWarp($warp);
-		                unset(WarpGUI::getInstance()->editwarp[$player->getName()]);
-		                WarpGUI::getInstance()->editwarp[$player->getName()] = $args[1];
-		                $player->sendMessage("§aSuccessfully rename warp to $args[1]");
-		            }else{
-                                $player->sendMessage("§cWarp name $args[1] is already exists");
+			   	]);
+		   		WarpGUI::getInstance()->getWarp()->save();
+		   		WarpGUI::getInstance()->removeWarp($warp);
+		           unset(WarpGUI::getInstance()->editwarp[$player->getName()]);
+		           WarpGUI::getInstance()->editwarp[$player->getName()] = $args[1];
+		           $player->sendMessage("§9[ §aSuccessfully §9]§r§e You have renamed the warp to §7$args[1]");
+		           }else{
+                                $player->sendMessage("§9[ §4ERROR §9]§r§c Warp name $args[1] is already exists");
 		            }
                 break;
                 case "item":
                   $item = $player->getInventory()->getItemInHand();
-                    if($item instanceof ItemBlock && $item instanceof Item){
+                    if($item instanceof ItemBlock or $item instanceof Item){
                     	$name = str_replace([" "], ["_"], strtolower($player->getInventory()->getItemInHand()->getVanillaName()));
                     	WarpGUI::getInstance()->getWarp()->set($warp, [
                             "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
@@ -93,48 +93,50 @@ class EventListener implements Listener {
                             "slot" => WarpGUI::getInstance()->getWarp()->get($warp)["slot"]
                     	]);
                         WarpGUI::getInstance()->getWarp()->save();
-                    	$player->sendMessage("§aSuccessfully update item warp in gui");
-		    } else {
-		       if($item->getTypeId() === BlockTypeIds::AIR){
-			  $player->sendMessage("Please hold an item in your hand!");
-			   return;
-		       }
-		    }
+                    	$player->sendMessage("§9[ §aSuccessfully §9]§r§e You have updated item warp in gui");
+                    }
                 break;
                 case "slot":
                     if(isset($args[1])){
 		                if(!is_numeric($args[1])){
-		                	$player->sendMessage("§cUsage:§7 slot <int>");
+		                	$player->sendMessage("§cUsage:§7 slot <int: 0-53>");
 		                	return;
 		                }
 		                if($args[1] >= 0 || $args[1] <= 53){
 		                	$slot = (int)$args[1];
-		                    if(!WarpGUI::getInstance()->getWarp()->exists($args[1])){
-		                        WarpGUI::getInstance()->getWarp()->set($warp, [
+		                	$all_warp = WarpGUI::getInstance()->getWarp()->getAll();
+		                	foreach($all_warp as $data => $key){
+		                	  if(!$args[1] === $all_warp[$data]["slot"]){
+		                	    $player->sendMessage("§9[ §4ERROR §9]§r§c That slot already exists, please try again");
+		                	    return;
+		                	  }else{
+		                    WarpGUI::getInstance()->getWarp()->set($warp, [
                                              "position" => WarpGUI::getInstance()->getWarp()->get($warp)["position"],
                                              "world" => WarpGUI::getInstance()->getWarp()->get($warp)["world"],
                                              "item" => WarpGUI::getInstance()->getWarp()->get($warp)["item"],
                                              "slot" => $slot
                             	        ]);
-					WarpGUI::getInstance()->getWarp()->save();
-		                        $player->sendMessage("§aSuccessfully update slot item in warp to $slot");
-		                    }else{
-                                       $player->sendMessage("§cThe warp you are editing no longer exists in the data");
-		                    }
+				            	WarpGUI::getInstance()->getWarp()->save();
+		                        $player->sendMessage("§9[ §aSuccessfully §9]§r§e You have updated slot item in warp to $slot");
+		                        return;
+		                	  }
+		              	  }
 		                }else{
-		                    $player->sendMessage("§cThe number you enter can only be entered from 0 -> 53");
+		                    $player->sendMessage("§9[ §4ERROR §9]§r§c The number you enter can only be entered from 0 -> 53");
+		                  return;
 		                }
 		            }else{
-		            	$player->sendMessage("§cUsage:§7 slot <int>");
+		            	$player->sendMessage("§cUsage:§7 slot <int: 0-53>");
+		            	return;
 		            }
                 break;
                 case "done":
                  if(WarpGUI::getInstance()->getWarp()->get($warp)["item"] == "air"){
-                    $player->sendMessage("§9[§4!§9]§c The item you setup is air. Please setup again!");
+                    $player->sendMessage("§9[ §4ERROR §9]§r§c The item you setup is air. Please setup again!");
                     return;
                  }
-                 if(WarpGUI::getInstance()->getWarp()->get($warp)["position"] == null || WarpGUI::getInstance()->getWarp()->get($warp)["slot"] == null || WarpGUI::getInstance()->getWarp()->get($warp)["item"] == null){
-                    $player->sendMessage("§l§9[§4!§9]§c Maybe you haven't set up: §b<position | item | slot>. Please check and try again!");
+                 if((WarpGUI::getInstance()->getWarp()->get($warp)["position"] == null) or (WarpGUI::getInstance()->getWarp()->get($warp)["item"] == null) or (WarpGUI::getInstance()->getWarp()->get($warp)["slot"] == "")){
+                    $player->sendMessage("§9[ §4ERROR §9]§r§c Maybe you haven't set up: §b<position | item | slot>. §cPlease check and try again!");
                  } else {
                     if(isset(WarpGUI::getInstance()->editwarp[$player->getName()])){
                     	unset(WarpGUI::getInstance()->editwarp[$player->getName()]);
